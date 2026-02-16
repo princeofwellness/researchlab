3135f8ad88820c42cbb555d5d552c5435f205505
Prince of Wellness <tieplee@sorrywecan.com>
2026-02-16 00:41:47 -0600

    style: bump TB/Raiffeisen/Sennheiser logos to h-10 md:h-12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -304,10 +304,10 @@ export default function HomePage() {
 
     const partners = [
         { name: "Meta", logo: "/logos/meta.png", size: "h-6 md:h-7" },
-        { name: "Tatra Banka", logo: "/logos/tatrabanka.png", size: "h-8 md:h-9" },
-        { name: "Raiffeisen", logo: "/logos/raiffeisen.png", size: "h-8 md:h-9" },
+        { name: "Tatra Banka", logo: "/logos/tatrabanka.png", size: "h-10 md:h-12" },
+        { name: "Raiffeisen", logo: "/logos/raiffeisen.png", size: "h-10 md:h-12" },
         { name: "VUB Banka", logo: "/logos/vub.png", size: "h-6 md:h-7" },
-        { name: "Sennheiser", logo: "/logos/sennheiser.png", size: "h-8 md:h-9" },
+        { name: "Sennheiser", logo: "/logos/sennheiser.png", size: "h-10 md:h-12" },
         { name: "Forbes", logo: "/logos/forbes.png", size: "h-6 md:h-7" },
         { name: "Audi", logo: "/logos/audi.svg", size: "h-6 md:h-7" }
     ]

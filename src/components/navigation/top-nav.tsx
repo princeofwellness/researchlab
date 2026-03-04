a0c8c94b6f1260d0c6a313d65544205a28b9ae0b
Prince of Wellness <tieplee@sorrywecan.com>
2026-03-04 22:10:16 +0100

    Clean nav: remove Communitea/Publications/Toolkit, add /mindshift route, simplify home

--- src/components/navigation/top-nav.tsx
+++ src/components/navigation/top-nav.tsx
@@ -37,12 +37,9 @@ export function TopNav({
 
     const navItems = [
         { name: "Home", href: "/" },
-        { name: "Communitea", href: "/communitea" },
-        { name: "Publications", href: "/publications" },
         { name: "Corporate", href: "/corporate" },
         { name: "THE MINDSHIFT", href: "/mindshift" },
         { name: "Lab", href: "/lab" },
-        { name: "Toolkit", href: "/shift" },
     ]
 
     const isDark = variant === "dark"
